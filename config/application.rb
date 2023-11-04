ad84f7d6c33f458ffd622842080828f41cb3fc11
Cade Hansen <codfish81@byui.edu>
2023-11-03 21:05:34 -0600

    changed applicationrb

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "rails/test_unit/railtie"
+require 'mongoid'
+
+Mongoid.load!(File.expand_path('mongoid.yml', './config'))
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
